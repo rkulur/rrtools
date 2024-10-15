1af4be37cbc833bd626ba57f14dcecac0f4d43db
rkulur <rashwithkulur07@gmail.com>
2024-10-15 19:57:30 +0530

    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    ENV_KEY: process.env.ENV_KEY,
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      TOOLS_API: JSON.stringify(env.TOOLS_API),
+    },
+  };
 });
