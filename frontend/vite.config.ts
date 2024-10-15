a663c044d3c9978107251ae91eac78d877d569fd
rkulur <rashwithkulur07@gmail.com>
2024-10-15 20:27:41 +0530

    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,7 @@ export default defineConfig(() => {
   return {
     plugins: [react()],
     define: {
-      "process.env.VITE_APP_TOOLS_API": JSON.stringify(
-        process.env.VITE_APP_TOOLS_API,
-      ),
+      "process.env.VITE_TOOLS_API": JSON.stringify(process.env.VITE_TOOLS_API),
     },
   };
 });
